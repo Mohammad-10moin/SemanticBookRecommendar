89ddbd5a2b1c1e62a05fc6c746322985821e9751
Mohammad Moeenuddin <moinuddin10code@gmail.com>
2025-04-27 21:58:45 +0530

    Update and rename gradio-dashboard.py to app.py

--- book-recommender/app.py
+++ book-recommender/app.py
@@ -3,23 +3,18 @@ import pickle
 import numpy as np
 import pandas as pd
 import gradio as gr
-from dotenv import load_dotenv
 
 # === LangChain and Chroma imports ===
 from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import CharacterTextSplitter
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_chroma import Chroma  # <- using new maintained import
-
-# === Load environment variables ===
-load_dotenv()
-
+from langchain_chroma import Chroma  
 # === Constants ===
 CACHE_FILE = "cached_documents.pkl"
 CHROMA_DB_DIR = "chroma_books_db"
 TEXT_FILE_PATH = "tagged_description.txt"
 
-# === Load books data ===
+# === Loading books and attaching thumbnails ===
 books = pd.read_csv('books_with_emotions.csv')
 books['large_thumbnail'] = books['thumbnail'] + "&fife=w800"
 books['large_thumbnail'] = np.where(
